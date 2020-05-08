e9f13287a9b0676d77bc9e2b87cdd02381fee85c
bekjan <bekambkg@gmail.com>
2020-05-08 15:49:50 +0600

    added .mvn

--- src/main/java/kg/bekhub/school/web/UserController.java
+++ src/main/java/kg/bekhub/school/web/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/users")
 public class UserController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserController(UserRepository userRepository) {
